cf4fdaf90bfddc8b941f09209fb1ffb400b82628
superman285 <superman285@126.com>
2019-03-28 09:16:00 +0800

    测试用例改写为正确版本 新加入重启测试用例

--- test/contract/emptyBoard_test.js
+++ test/contract/emptyBoard_test.js
@@ -1,15 +1,12 @@
 let TicTacToe = artifacts.require("TicTacToe");
 
 contract("TicTacToe", accounts => {
-  it("chessboard should be empty at the start", async () => {
-    let instance = await TicTacToe.new();
-    console.log('合约实例:\n', instance);
-    let createRes = await instance.createGame({from: accounts[0], value: web3.utils.toWei("0.1", "ether")});
-    console.log('创建游戏res:\n', createRes);
-    assert.equal(createRes.logs[0].event, "GameCreated", "The game must be created successfully！");
-    let chessboard = await instance.getWholeBoard.call();
-    console.log('游戏棋盘:\n', chessboard);
-    assert.equal(chessboard[0][0], "", "The first position must be empty!");
-    console.log('test success!');
-  })
+    it("chessboard should be empty at the start",async ()=>{
+        let instance = await TicTacToe.new({from: accounts[0], value: web3.utils.toWei("0.1", "ether")});
+        console.log('合约实例:\n',instance);
+        let chessboard = await instance.getWholeBoard.call();
+        console.log('游戏棋盘:\n',chessboard);
+        assert.equal(chessboard[0][0],"","The first position must be empty!");
+        console.log('test success!');
+    })
 });
