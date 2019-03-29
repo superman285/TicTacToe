509f8286af66d38747bbb046b7fcaa8795a185f7
superman285 <superman285@126.com>
2019-03-29 14:00:38 +0800

    更新dist

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,9 @@
 module.exports = {
   publicPath: "./",
-
   devServer: {
     port: 3000,
     compress: true // 服务器返回浏览器的时候是否启动gzip压缩
   },
   productionSourceMap: false,
-
   configureWebpack: {}
 };
